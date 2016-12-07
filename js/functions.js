702e9727c1485e9b401faaf3fe1d5cf55925b9f3
juliani91 <juliani91@hotmail.com>
2016-12-07 15:38:44 -0600

    First commit

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+$(window).scroll(function(){
+
+
+});
+
+
+$(document).ready(function(){
+
+
+$(".hamb").click(function(){
+  $(".menu-list").toggleClass("active");
+  $(".spanf ").toggleClass("span1 ");
+  $(".spans ").toggleClass("span2 ");
+  $(".spant ").toggleClass("span3 ");
+});
+
+
+
+});
